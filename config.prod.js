af42385017c7624f97efcbe7e0c275e8a5e079dc
root <chaiyapon.kat@murata.com>
2025-12-16 15:31:48 +0700

    backup file

---
+++ config.prod.js
@@ -0,0 +1,5 @@
+// Production configuration
+window.APP_CONFIG = {
+    API_URL: 'http://mtl2app-dkr-reg.co.murata.local:3000/api',  // Will be relative to the current domain
+    SOCKET_URL: 'http://mtl2app-dkr-reg.co.murata.local:3000'    // Empty string will connect to current host
+};
