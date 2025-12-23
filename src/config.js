aea4fd827fe5dc384043cb8faa589174ec19ca2e
root <chaiyapon.kat@murata.com>
2025-12-23 11:27:49 +0700

    change config.js socket url

--- src/config.js
+++ src/config.js
@@ -19,7 +19,7 @@ export async function loadConfig() {
         ...defaultConfig, 
         ...config,
         // Ensure SOCKET_URL is set correctly based on environment
-        SOCKET_URL: config.SOCKET_URL || (isDevelopment ? 'http://mtl2app-dkr-reg.co.murata.local:3001' : window.location.origin)
+        SOCKET_URL: config.SOCKET_URL || (isDevelopment ? 'https://webapp-vote-server.onrender.com' : window.location.origin)
       };
     }
   } catch (error) {
