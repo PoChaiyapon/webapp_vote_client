fc7e23e552de05dca25b75802eeae61f02cbd8fe
root <chaiyapon.kat@murata.com>
2025-12-23 09:23:50 +0700

    clear all source

--- config.js
+++
@@ -1,5 +0,0 @@
-// Production configuration
-window.APP_CONFIG = {
-    API_URL: 'https://webapp-vote-server.onrender.com/api',  // Will be relative to the current domain
-    SOCKET_URL: 'https://webapp-vote-server.onrender.com'    // Empty string will connect to current host
-};
